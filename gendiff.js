c772ea4c32ebf5816e4cf8d240c67a41c56efe4d
gradrrs <artemlal1998@gmail.com>
2025-12-16 14:28:39 +0300

    2

--- gendiff.js
+++ gendiff.js
@@ -1,10 +1,22 @@
 #!/usr/bin/env node
-import { Command } from "commander";
+
+import { Command } from 'commander';
 
 const program = new Command();
 
 program
-    .description(`Compares two configuration files and shows a difference.`)
-    .option('-V, --version', 'output the version number');
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .argument('<filepath1>', 'path to first file')
+  .argument('<filepath2>', 'path to second file')
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2, options) => {
+    console.log(`Comparing ${filepath1} and ${filepath2}`);
+    if (options.format) {
+      console.log(`Format: ${options.format}`);
+    }
+    console.log('Difference will be shown here');
+  });
 
 program.parse();
---
+++ src/index.js
@@ -0,0 +1,5 @@
+const genDiff = (filepath1, filepath2) => {
+  return 'Comparison result will be here';
+};
+
+export default genDiff;
